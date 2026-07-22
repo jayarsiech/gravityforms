7178a776c8d6626135c020c960ccd717830bdde6
jayarsiech <jayquery768@gmail.com>
2026-07-22 14:34:03 +0330

    Update gform_get_form_filter_13.php

--- gform_get_form_filter_13.php
+++ gform_get_form_filter_13.php
@@ -23,3 +23,28 @@ function custom_schedule( $form_string, $form ) {
     return $form_string;
 }
 ?>
+// مثال بعدی
+
+<?php
+add_filter( 'gform_get_form_filter_64', 'yekbar_reserv_mosahebe', 10, 2 );
+function yekbar_reserv_mosahebe( $form_string, $form ) {
+    
+    $current_user = wp_get_current_user();
+
+    $search_criteria = array(
+        'status'        => 'active',
+        'field_filters' => array( //which fields to search
+            array(
+                'key' => 'created_by', 'value' => $current_user->ID, //Current logged in user
+            )
+        )
+    );
+    $form_id = 64;
+    $entry = GFAPI::get_entries($form_id,$search_criteria);
+
+    if ( !empty($entry) ) {
+$form_string = '<p></p>';
+    }   
+    return $form_string;
+}
+?>
